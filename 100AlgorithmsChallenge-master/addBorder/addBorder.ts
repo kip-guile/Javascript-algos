28fcc7e2b21657c85a651fae65e7bf63ceb9254e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-20 17:51:05 +0100

    master

--- 100AlgorithmsChallenge-master/addBorder/addBorder.ts
+++ 100AlgorithmsChallenge-master/addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 function addBorder(picture: string[]): string[] {
-
+  let newArr = []
+  let wall = ''
+  for (let i = 0; i < picture[0].length + 2; i++) {
+    wall = wall.concat('*')
+  }
+  newArr.push(wall)
+  for (let i = 0; i < picture.length; i++) {
+    newArr.push('*'.concat(picture[i], '*'))
+  }
+  newArr.push(wall)
+  return newArr
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']))
