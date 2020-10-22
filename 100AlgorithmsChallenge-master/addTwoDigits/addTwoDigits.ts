65d3c665358dbf9886ddee407cd7aac1d5f9408f
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-22 07:48:11 +0100

    addtwodigits

--- 100AlgorithmsChallenge-master/addTwoDigits/addTwoDigits.ts
+++ 100AlgorithmsChallenge-master/addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
 function addTwoDigits(n: any): number {
-
+  let arr = n.toString().split('')
+  return arr.reduce((acc, next) => {
+    return parseInt(acc) + parseInt(next)
+  })
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(59))
