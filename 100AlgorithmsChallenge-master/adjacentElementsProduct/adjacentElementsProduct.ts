046e7e2d6c6631d3aad0e199d2452cf8470d0435
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-22 08:04:45 +0100

    adjacent elements products

--- 100AlgorithmsChallenge-master/adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100AlgorithmsChallenge-master/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+  let res = 0
+  for (let i = 0; i < inputArray.length; i++) {
+    const check = inputArray[i] * inputArray[i + 1]
+    if (check > res) {
+      res = check
+    }
+  }
+  return res
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
