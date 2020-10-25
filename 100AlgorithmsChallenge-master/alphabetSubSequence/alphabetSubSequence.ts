8d9a6e44dc841af437b69753c3a8998fad0e1a23
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-25 18:49:02 +0100

    alternatingSUms

--- 100AlgorithmsChallenge-master/alphabetSubSequence/alphabetSubSequence.ts
+++ 100AlgorithmsChallenge-master/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,14 @@
 function alphabetSubsequence(s: string): boolean {
-
+    let cache = {}
+    'abcdefghijklmnopqrstuvwxyz'.split('').forEach((letter, i) => {
+        cache[letter] = i+1
+    })
+    for (let i = 0; i < s.length - 1; i++) {
+        if (cache[s[i]] >= cache[s[i+1]]) {
+            return false
+        }
+    }
+    return true
 }
 
 console.log(alphabetSubsequence('zab'))
--- 100AlgorithmsChallenge-master/alternatingSums/alternatingSums.ts
+++ 100AlgorithmsChallenge-master/alternatingSums/alternatingSums.ts
@@ -1,5 +1,17 @@
 function alternatingSums(a: number[]): number[] {
-
+    let arr = []
+    let team1sum = 0
+    let team2sum = 0
+    a.forEach((number, i) => {
+        if (i % 2 === 0) {
+            team1sum += number
+        } else {
+            team2sum += number
+        }
+    })
+    arr.push(team1sum)
+    arr.push(team2sum)
+    return arr
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
