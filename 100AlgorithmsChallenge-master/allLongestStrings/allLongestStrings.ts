7b9df551dd496ff750996a8c94614c060a5e435a
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-25 15:52:23 +0100

    new changes

--- 100AlgorithmsChallenge-master/allLongestStrings/allLongestStrings.ts
+++ 100AlgorithmsChallenge-master/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+  let resArr = []
+  inputArray.forEach((str) => {
+    const strCount = str.length
+    if (resArr.length === 0) {
+      resArr.push(str)
+    } else if (strCount > resArr[0].length) {
+      resArr = []
+      resArr.push(str)
+    } else if (strCount === resArr[0].length) {
+      resArr.push(str)
+    }
+  })
+  return resArr
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(
+  allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba', 'parrot', 'p', 'oyttre'])
+)
