707676d9204c9a969c556f2eaae9bb59549dabf2
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-25 17:17:12 +0100

    alphabetShift

--- 100AlgorithmsChallenge-master/alphabeticShift/alphabeticShift.ts
+++ 100AlgorithmsChallenge-master/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,15 @@
 function alphabeticShift(inputString: string): string {
-
+    let mainStr = 'abcdefghijklmnopqrstuvwxyz'
+    let afterStr = 'bcdefghijklmnopqrstuvwxyza'
+    const cache = {}
+    let newStr = ''
+    mainStr.split('').forEach((letter, i) => {
+        cache[letter] = afterStr[i]
+    })
+    for (let i = 0; i < inputString.length; i++) {
+        newStr = newStr.concat(cache[inputString[i]])
+    }
+    return newStr
 }
 
 console.log(alphabeticShift('crazy'));
