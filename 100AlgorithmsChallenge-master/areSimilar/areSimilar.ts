d600730703ccf1c0ada25e857a7d6e087b029cd8
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-29 12:14:08 +0100

    arrayChange

--- 100AlgorithmsChallenge-master/areSimilar/areSimilar.ts
+++ 100AlgorithmsChallenge-master/areSimilar/areSimilar.ts
@@ -1,7 +1,26 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   //start
+   let cacheA = {}
+   let cacheB = {}
+   let errorCount = 0
+   a.forEach((num, i) => {
+        cacheA[i] = num
+   })
+   b.forEach((num, i) => {
+        cacheB[i] = num
+   })
+   
+   for (const key in cacheA) {
+       if (cacheA[key] !== cacheB[key]) {
+           errorCount += 1
+    }
+   }
+   if (errorCount > 2) {
+       return false
+   }
+   return true
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3, 4, 7], [1, 2, 4, 7, 3]));
--- 100AlgorithmsChallenge-master/arrayChange/arrayChange.ts
+++ 100AlgorithmsChallenge-master/arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let res = 0
+    inputArray.forEach((num, i) => {
+        if (num >= inputArray[i+1]) {
+            let difference = (num + 1) - inputArray[i + 1]
+            res += difference
+            inputArray[i+1] = inputArray[i+1] + difference
+        }
+    })
+    return res
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 4, 2]));
+console.log(arrayChange([1, 0, 5, 4, 2, 0, 1, 7]));
