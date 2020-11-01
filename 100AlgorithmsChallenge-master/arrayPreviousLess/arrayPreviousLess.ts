ab28e19608fae7e056d926856bb35fdc17caa946
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-01 21:14:37 +0100

    work

--- 100AlgorithmsChallenge-master/arrayPreviousLess/arrayPreviousLess.ts
+++ 100AlgorithmsChallenge-master/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,21 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+    let newArr = []
+    for (let i = 0; i < items.length; i++) {
+        if (i = 0) {
+            newArr.push(-1)
+            continue
+        }
+        for (let j = i-1; j < 1; j-=1) {
+            if (items[j] === -1) {
+                continue
+            } else if (items[i] > items[j]) {
+                newArr.push(items[j])
+            } else {
+                items[i] = -1
+            }
+        }        
+    }
+    return newArr
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
