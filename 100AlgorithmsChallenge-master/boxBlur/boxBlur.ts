101d02d0b5913be3d8d21eedefa13e7729d5a8f5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-25 23:09:25 +0100

    iscasesensitivepalindrome0.1

--- 100AlgorithmsChallenge-master/boxBlur/boxBlur.ts
+++ 100AlgorithmsChallenge-master/boxBlur/boxBlur.ts
@@ -1,11 +1,13 @@
 function boxBlur(image: number[][]): number[][] {
     let sum = 0
+    let count = 0
     for (let i = 0; i < image.length; i++) {
         for (let j = 0; j < image[i].length; j++) {
             sum += image[i][j]
+            count += 1
         }
     }
-    return [[Math.floor(sum/9)]]
+    return [[Math.floor(sum/count)]]
 }
 
 console.log(boxBlur([[1, 1, 1], 
--- 100AlgorithmsChallenge-master/candies/candies.ts
+++ 100AlgorithmsChallenge-master/candies/candies.ts
@@ -2,4 +2,4 @@ function candies(n: number, m: number): number {
     return Math.floor(m/n) * n
 }
 
-console.log(candies(3, 10));
+console.log(candies(4, 14));
