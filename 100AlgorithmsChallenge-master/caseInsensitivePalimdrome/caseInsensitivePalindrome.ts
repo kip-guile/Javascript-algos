185319bff61d36a707c853f9b1ab124269c8de91
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-25 23:08:41 +0100

    iscasesensitivepalindrome

--- 100AlgorithmsChallenge-master/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ 100AlgorithmsChallenge-master/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,13 @@
-function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+function isCaseInsensitivePalindrome(inputString: string, count=0, last=inputString.length - 1): boolean {
+    if (inputString[count].toLowerCase() !== inputString[last].toLowerCase()) {
+        return false
+    } else if (count === inputString.length - 1) {
+        return true
+    }
+    count = count + 1
+    last = last - 1
+    return isCaseInsensitivePalindrome(inputString, count, last)
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
-console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('abAAba'));
