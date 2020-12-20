588c0409122159c8fa87e81410aa7284847977e1
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-12-20 19:53:20 +0100

    feat: chessboard

--- 100AlgorithmsChallenge-master/chessBoardCellColor/chessBoardCellColor.ts
+++ 100AlgorithmsChallenge-master/chessBoardCellColor/chessBoardCellColor.ts
@@ -1,6 +1,23 @@
 function chessBoardCellColor(cell1: string, cell2: string): boolean {
-
+  let board = {}
+  'ABCDEFGH'.split('').forEach((letter, i) => {
+    if (i % 2 !== 0) {
+      board[letter] = true
+    } else {
+      board[letter] = false
+    }
+  })
+  let cell1position = parseInt(cell1[1])
+  let cell2position = parseInt(cell2[1])
+  let cell1color = cell1position % 2 !== 0 ? board[cell1[0]] : !board[cell1[0]]
+  let cell2color = cell2position % 2 !== 0 ? board[cell2[0]] : !board[cell2[0]]
+  console.log(cell1position, cell2position)
+  if (cell1color === cell2color) {
+    return true
+  } else {
+    return false
+  }
 }
 
-console.log(chessBoardCellColor('A1', 'C3'));
-console.log(chessBoardCellColor('A1', 'H3'));
+console.log(chessBoardCellColor('A1', 'C3'))
+console.log(chessBoardCellColor('A1', 'H3'))
