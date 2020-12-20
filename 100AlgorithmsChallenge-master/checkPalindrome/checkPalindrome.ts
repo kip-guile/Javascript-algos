64dd06961a99e6aa5599a4739e475e68d02e0dc9
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-12-20 13:10:36 +0100

    ispalindrome

--- 100AlgorithmsChallenge-master/checkPalindrome/checkPalindrome.ts
+++ 100AlgorithmsChallenge-master/checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,25 @@
-function checkPalindrome(inputString: string): boolean {
-
+function checkPalindrome(inputString: string, pointer1 = 0): boolean {
+  let pointer2
+  if (pointer1 === 0) {
+    pointer2 = inputString.length - 1
+  } else {
+    pointer2 = inputString.length - 1 - pointer1
+  }
+  if (
+    pointer1 === inputString.length &&
+    inputString[pointer1] === inputString[pointer2]
+  ) {
+    return true
+  }
+  if (inputString[pointer1] !== inputString[pointer2]) {
+    return false
+  }
+  let newpointer1 = pointer1 + 1
+  return checkPalindrome(inputString, newpointer1)
 }
 
-console.log(checkPalindrome('aabaa'));
-console.log(checkPalindrome('abac'));
-console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'))
+console.log(checkPalindrome('abba'))
+console.log(checkPalindrome('abac'))
+console.log(checkPalindrome('abbaabba'))
+console.log(checkPalindrome('a'))
