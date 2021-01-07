27b5b0321508826828aad1ac7096a9554d922ff5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2021-01-07 19:59:04 +0100

    feat: chunky monkey

--- 100AlgorithmsChallenge-master/chunkyMonkey/chunkyMonkey.ts
+++ 100AlgorithmsChallenge-master/chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,18 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
-
+  let base = []
+  for (let i = 0; i < arr.length; i++) {
+    let newArr = []
+    for (let j = 0; j < size; j++) {
+      if (arr.length < 1) {
+        continue
+      }
+      let popped = arr.shift()
+      newArr.push(popped)
+    }
+    base.push(newArr)
+  }
+  return base
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2))
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4))
