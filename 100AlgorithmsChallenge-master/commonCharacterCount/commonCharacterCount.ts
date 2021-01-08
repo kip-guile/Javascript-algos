2d1c8b756fdd9e2a329422af635006387bee60e1
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2021-01-08 12:53:36 +0100

    feat: common character count

--- 100AlgorithmsChallenge-master/commonCharacterCount/commonCharacterCount.ts
+++ 100AlgorithmsChallenge-master/commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,24 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+  let count = 0
+  const memoize = (string) => {
+    let cache = {}
+    for (let i = 0; i < string.length; i++) {
+      if (cache[string[i]]) {
+        cache[string[i]] += 1
+      } else {
+        cache[string[i]] = 1
+      }
+    }
+    return cache
+  }
+  let cacheA = memoize(s1)
+  let cacheB = memoize(s2)
+  for (const key in cacheA) {
+    if (cacheB.hasOwnProperty(key)) {
+      count += Math.min(cacheB[key], cacheA[key])
+    }
+  }
+  return count
 }
 
-console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount('aabcdc', 'adcaa'))
