64898ac0ef83c797b955e8c457a84c1e6fecd598
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2021-02-14 21:06:34 +0100

    valid time

--- 100AlgorithmsChallenge-master/validTime/validTime.ts
+++ 100AlgorithmsChallenge-master/validTime/validTime.ts
@@ -1,7 +1,28 @@
 function validTime(time: string): boolean {
+  let h = ''
+  let m = ''
+  let count = 0
+  let arr = time.split('')
+  for (let i = 0; i < arr.length; i++) {
+    if ((typeof parseInt(arr[i]) === 'number' && parseInt(arr[i]) % 1) === 0) {
+      if (count < 2) {
+        h = h + arr[i]
+        count += 1
+      } else {
+        m = m + arr[i]
+      }
+    }
+  }
+  if (parseInt(h) > 24 || parseInt(h) < 0) {
+    return false
+  }
+  if (parseInt(m) > 60 || parseInt(h) < 0) {
+    return false
+  }
 
+  return true
 }
 
-console.log(validTime('13:58'));
-console.log(validTime('25:51'));
-console.log(validTime('02:76'));
+console.log(validTime('13:58'))
+console.log(validTime('25:51'))
+console.log(validTime('02:76'))
